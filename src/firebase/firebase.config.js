2d1c9b4a3a36999619275d0f7fef4945fe9b99ad
Moinuddin-dotcom <moinchy7@gmail.com>
2024-12-23 03:00:13 +0600

    firbase updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2YK6BUAIefhqatxE5fklCzxRFDyuHeDE",
-  authDomain: "hotel-booking-a11.firebaseapp.com",
-  projectId: "hotel-booking-a11",
-  storageBucket: "hotel-booking-a11.firebasestorage.app",
-  messagingSenderId: "196554679914",
-  appId: "1:196554679914:web:7b815d5a871641b67b4c13"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
